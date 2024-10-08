1c61e724e4c71a276a7ca03a50e53b69011e2b7a
Gilvan <87739497+GilvanPedro@users.noreply.github.com>
2024-10-08 18:24:58 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,12 @@
+window.sr = ScrollReveal({reset:true});
+
+sr.reveal('.container', {duration: 1000});
+sr.reveal('.quemsomos', {duration: 1000});
+sr.reveal('.banner', {duration: 1000});
+sr.reveal('#esp', {duration: 1000});
+sr.reveal('.convi', {duration: 1000});
+sr.reveal('.margem', {duration: 1000});
+sr.reveal('.infos', {duration: 1000});
+sr.reveal('.ingresso', {duration: 1000});
+sr.reveal('.roupa', {duration: 1000});
+sr.reveal('.botao', {duration: 1000});
